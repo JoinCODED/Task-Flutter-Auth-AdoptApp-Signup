a15796e641a89dc1b0f1a454b93d948b623b7369
ZainabBaq <52189918+ZainabBaq@users.noreply.github.com>
2022-06-22 01:57:46 +0300

    ready for auth

--- lib/models/pet.dart
+++ lib/models/pet.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'pet.g.dart';
+
+@JsonSerializable()
 class Pet {
   int? id;
   String name;
@@ -14,11 +19,6 @@ class Pet {
       required this.age,
       required this.gender});
 
-  Pet.fromJson(Map<String, dynamic> json)
-      : id = json['id'] as int?,
-        name = json['name'] as String,
-        adopted = json['adopted'] as bool,
-        image = json['image'] as String,
-        age = json['age'] as int,
-        gender = json['gender'] as String;
+  factory Pet.fromJson(Map<String, dynamic> json) => _$PetFromJson(json);
+  Map<String, dynamic> toJson() => _$PetToJson(this);
 }
